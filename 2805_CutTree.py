9f633e50496299f11413945478b5ca1bd96e0393
pdh93621 <84228242+pdh93621@users.noreply.github.com>
2021-07-05 22:04:01 +0900

    Add files via upload

---
+++ 2805_CutTree.py
@@ -0,0 +1,22 @@
+N, M = list(map(int, input().split()))
+
+trees = list(map(int, input().split()))
+
+start = min(trees)
+end = max(trees)
+result = 0
+
+while start <= end:
+    mid = (start + end) // 2
+    temp = 0
+    for tree in trees:
+        if tree > mid:
+            temp += tree - mid
+    
+    if temp >= M:
+        start = mid + 1
+        result = mid
+    else:
+        end = mid - 1
+    
+print(result)
