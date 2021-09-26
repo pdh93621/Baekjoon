f6da369f4a39ca3b4479c1698801ba108a05d759
pdh93621 <hyenp1993@gmail.com>
2021-09-26 19:22:48 +0900

    no message

---
+++ 2588_Multiplication.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main(){
+    int num1;
+    int num2;
+
+    cin >> num1;
+    cin >> num2;
+
+    for(int i = 0; i < 3; i++){
+        cout << i;
+    }
+    return 0;
+}
