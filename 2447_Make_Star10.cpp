e0d4bf1fb88b3ec9eac9be52aa4d107cbab22412
pdh93621 <hyenp1993@gmail.com>
2021-09-27 16:21:01 +0900

    no message

---
+++ 2447_Make_Star10.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+void inflate(char (*stars)[2200] , int num){
+    int temp = num / 3;
+    for(int i = 0; i < num; i++){
+        for(int j = 0; j < num; j++){
+            if ((i >= num / 3 and i < 2 * num / 3) and (j >= num / 3 and j < 2 * num / 3)){
+                stars[i][j] = ' '; 
+            }
+            else {
+                stars[i][j] = stars[i % temp][j % temp];
+            }
+        }
+    }
+}
+
+int main(){
+    int N;
+    cin >> N;
+    char stars[2200][2200];
+    char star = '*';
+    stars[0][0] = star;
+
+    int num = 3;
+    while (num <= N){
+        inflate(stars, num);
+        num *= 3;
+    }
+
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < N; j++){
+            cout << stars[i][j];
+        }
+        cout << endl;
+    }
+    return 0;
+}
