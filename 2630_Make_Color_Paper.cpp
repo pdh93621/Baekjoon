b9605e6d8a63d772d07ac3e8aa812b747249f88c
pdh93621 <hyenp1993@gmail.com>
2021-10-20 00:29:46 +0900

    no message

--- 2630_Make_Color_Paper.cpp
+++ 2630_Make_Color_Paper.cpp
@@ -3,16 +3,29 @@
 
 using namespace std;
 
-pair<int, int> solution(int matrix[128][128], int N){
-    pair<int, int> result = make_pair(0, 0);
+pair<int, int> divide_blocks(int matrix[][128], int row, int column, int n){
 
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < N; j++){
-            cout << matrix[i][j] << " ";
-        }
-        cout << endl;
+    if(n == 1){
+        return make_pair(!matrix[row][column], matrix[row][column]);
     }
     
+    pair<int, int> result = make_pair(0, 0);
+    pair<int, int> temp;
+
+    for(int i = 0; i < 2; i++){
+        for(int j = 0; j < 2; j++){
+            temp = divide_blocks(matrix, row + i * n / 2, column + j * n / 2, n / 2);
+            result.first += temp.first;
+            result.second += temp.second;
+        }
+    }
+
+    //cout << n << " " << "1st: " << result.first << " " << "2nd: " << result.second <<endl;
+
+    if(!(result.first * result.second)){
+        return make_pair((bool)result.first, (bool)result.second);
+    }
+
     return result;
 }
 
@@ -20,7 +33,7 @@ int main(){
     int matrix[128][128] = {0,};
     int N;
     pair<int, int> answer;
-    cin >> N;
+    cin >> N; 
 
     for(int i = 0; i < N; i++){
         for(int j = 0; j < N; j++){
@@ -28,9 +41,10 @@ int main(){
         }
     }
 
-    answer = solution(matrix, N);
+    answer = divide_blocks(matrix, 0, 0, N);
 
-    cout << answer.first << " " << answer.second << endl;
+    cout << answer.first << endl;
+    cout << answer.second << endl;
 
     return 0;
 }
---
+++ testtest.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <utility>
+
+using namespace std;
+
+int main(){
+
+    int k = 10;
+
+    bool l = k;
+
+    cout << (int) l << endl;
+
+    return 0;
+}
