7008cab574946c599a778d886c6835052d55a78d
pdh93621 <hyenp1993@gmail.com>
2021-10-22 21:12:14 +0900

    no message

--- 1992_Quad_Tree.cpp
+++ 1992_Quad_Tree.cpp
@@ -9,12 +9,22 @@ string divide_part(int matrix[][64], int row, int column, int N){
     }
     
     string result = "";
+    string temp;
+    
     for(int i = 0; i < 2; i++){
         for(int j = 0; j < 2; j++){
-            
+            temp = divide_part(matrix, row + i * N / 2, column + j * N / 2, N / 2);
+            result += temp;
         }
     }
-    
+
+    if(result == "1111" || result == "0000"){
+        string a = ""; 
+        a.push_back(result[0]);
+        return a;
+    }
+
+    return "(" + result + ")";
 }
 
 int main(){
@@ -29,12 +39,7 @@ int main(){
         }
     }
 
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < N; j++){
-            cout << matrix[i][j] << " ";
-        }
-        cout << endl;
-    }
+    cout << divide_part(matrix, 0 , 0, N) << endl;
 
     return 0;
 }
