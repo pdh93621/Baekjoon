0bd3ea58e3d9aa53fac4e38db1b835d2db96991f
pdh93621 <hyenp1993@gmail.com>
2021-10-25 02:58:39 +0900

    no message

--- 1874_Stack_Seq.cpp
+++ 1874_Stack_Seq.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
 
 int main(){
+    int n;
+    cin >> n;
+    int now = 1;
+    stack<int> seq;
+    string answer;
+    
+    while(n--){
+        int temp;
+        cin >> temp;
 
+        while(now <= temp){
+            seq.push(now++);
+            answer += '+';
+        }
+
+        if(temp == seq.top()){
+            seq.pop();
+            answer += '-';
+        }
+        else{
+            cout << "NO" << endl;
+            return 0;   
+        }
+    }
+
+    for(auto i : answer){
+        cout << i << '\n';
+    }
     return 0;
 }
