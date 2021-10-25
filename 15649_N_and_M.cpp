23629ad5b593d485a7706dbb7034f27c0cbdd62b
pdh93621 <hyenp1993@gmail.com>
2021-10-26 02:34:39 +0900

    no message

--- 15649_N_and_M.cpp
+++ 15649_N_and_M.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
 
+using namespace std;
+
+bool visited[9];
+int temp[9];
+int N;
+int M;
+
+void dfs(int m){
+    if (m == M){
+        for(int i = 0; i < M; i++){
+            cout << temp[i] << ' ';
+        }
+        cout << endl;
+    }
+    else{
+        for(int i = 1; i <= N; i++){
+            if(!visited[i]){
+                temp[m] = i;
+                visited[i] = true;
+                dfs(m + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
+
 int main(){
-
-
+    cin >> N >> M;
+    dfs(0);
     return 0;
 }
--- testtest.cpp
+++ testtest.cpp
@@ -1,15 +1,26 @@
-#include <iostream>
-#include <utility>
+#include <iostream> 
+using namespace std; 
+#define MAX 9 
+int N,M; 
+int arr[MAX]; 
+bool visited[MAX]; 
+void dfs(int k) {
+    if(k==M) {
+        for(auto i =0;i<M;i++) cout << arr[i] << " "; 
+        cout << "\n"; 
+    }else {  
+        for(auto i=1; i<=N;i++) { 
+            if(!visited[i]) {
+                visited[i]=true; 
+                arr[k]=i; 
+                dfs(k+1); 
+                visited[i]=false; 
+            } 
+        } 
+    } 
+} 
 
-using namespace std;
-
-int main(){
-
-    int k = 10;
-
-    bool l = k;
-
-    cout << (int) l << endl;
-
-    return 0;
+int main() {
+     cin >> N >> M; 
+     dfs(0); 
 }
