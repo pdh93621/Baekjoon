e9c709c4bbd340ec223bf552377e5a2de9271992
pdh93621 <hyenp1993@gmail.com>
2021-11-02 23:53:17 +0900

    no message

--- 17298_NGE.cpp
+++ 17298_NGE.cpp
@@ -1,14 +1,35 @@
 #include <iostream>
+#include <vector>
 #include <stack>
+#include <utility>
 
 using namespace std;
 
 int main(){
     int N;
     cin >> N;
-    for(int i = 0; i < N; i++){
+    stack<pair<int, int>> s;
+    vector<int> nga(N);
+    int temp_n;
 
+    for(int i = 0; i < N; i++){
+        cin >> temp_n;
+        while(!s.empty() && s.top().second < temp_n){
+            nga[s.top().first] = temp_n;
+            s.pop();
+        }
+        s.push(make_pair(i, temp_n));
     }
+
+    while(!s.empty()){
+        nga[s.top().first] = -1;
+        s.pop();
+    }
+
+    for(auto i : nga){
+        cout << i << " ";
+    }
+    cout << "\n";
 
     return 0;
 }
