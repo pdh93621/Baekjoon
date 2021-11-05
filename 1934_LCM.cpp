63bd3e228ba2aec42ea1606208c843b26ad59946
pdh93621 <hyenp1993@gmail.com>
2021-11-05 20:08:31 +0900

    no message

---
+++ 1934_LCM.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int GCD(int small, int big){
+    int temp = big % small;
+    if(temp){
+        return GCD(temp, small);
+    }
+    else{
+        return small;
+    }
+}
+
+int main(){
+    int T;
+    int A,B;
+    cin >> T;
+
+    for(int i = 0; i < T; i++){
+        cin >> A;
+        cin >> B;
+        cout << A / GCD(min(A, B), max(A, B)) * B << "\n";
+    }
+    
+    
+
+    return 0;
+}
