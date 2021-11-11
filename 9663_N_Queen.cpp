f45ea3a8299db9cfe9dcd9bbdce7358ebcd09075
pdh93621 <hyenp1993@gmail.com>
2021-11-11 19:00:02 +0900

    no message

--- 9663_N_Queen.cpp
+++ 9663_N_Queen.cpp
@@ -2,12 +2,38 @@
 
 using namespace std;
 int N;
-int chess[15][15];
+int result;
+int col[16];
+
+bool not_meet(int n, int i){
+    for(int j = 1; j < n; j++){
+        if(col[n - j] == i || abs(col[n - j] - i) == j){
+            return false;
+        }
+    }
+    return true;
+}
+
+void dfs(int n){
+    if(n > N){
+        result++;
+        return;
+    }
+
+    for(int i = 1; i <= N; i++){
+        if(not_meet(n, i)){
+            col[n] = i;
+            dfs(n + 1);
+        }
+    }
+}
 
 int main(){
     cin >> N;
 
-    
+    dfs(1);
+
+    cout << result << endl;    
 
     return 0;
 }
