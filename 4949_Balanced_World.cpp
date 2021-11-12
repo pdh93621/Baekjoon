f282f6f4fcfda1703061d3cf7dd614697d2f6f5f
pdh93621 <hyenp1993@gmail.com>
2021-11-12 16:26:54 +0900

    no message

--- 4949_Balanced_World.cpp
+++ 4949_Balanced_World.cpp
@@ -4,26 +4,67 @@
 
 using namespace std;
 
-int main(){
-    char brakets[4] = {'(', ')', '[', ']'};
-    string str;
 
-    while(1){
-        stack<char> sta;
-        getline(cin, str);
 
-        if(str == "."){
-            break;
-        }
+int is_bracket(char s){
+    char brackets[4] = {'(', ')', '[', ']'};
+    for(int i = 0; i < 4; i++){
+        if(s == brackets[i]) return i;
+    }
+    return -1;
+}
 
-        for(auto s : str){
-            for(auto i : brakets){
-                if(s == i){
-                    
+bool solution(string str){
+    stack<int> k;
+
+    for(auto s: str){
+        int temp = is_bracket(s);
+        if(temp >= 0){
+            
+            if(k.empty()){
+                if(temp % 2){
+                    return false;
+                }
+                else{
+                    k.push(temp);
+                }
+            }
+            else{
+                int t = temp;
+                temp += k.top();
+                if(temp % 2){
+                    if(temp == 3){
+                        return false;
+                    }
+                    else{
+                        k.pop();
+                    }
+                }
+                else{
+                    k.push(t);
                 }
             }
         }
     }
 
+    return k.empty();
+}
+
+int main(){
+    string str;
+    while(1){
+        getline(cin, str);
+        
+        if(str == "."){
+            break;
+        }
+        
+        if(solution(str)){
+            cout << "yes" << "\n";
+        }
+        else{
+            cout << "no" << "\n";
+        }
+    }
     return 0;
 }
