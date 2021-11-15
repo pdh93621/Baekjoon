7b067e65dc2b4c0fb1217ea2a3b30e8daba07ec6
pdh93621 <hyenp1993@gmail.com>
2021-11-15 19:23:55 +0900

    no message

--- 13460_Marble_Escape.cpp
+++ 13460_Marble_Escape.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+int N, M;
+char board[10][10];
+int visited[10][10];
+
+int x_dir[4] = {1, -1, 0, 0};
+int y_dir[4] = {0, 0, 1, -1};
+
 int main(){
+
+    int red_x, red_y;
+    int blue_x, blue_y;
+
+    cin >> N >> M;
+
+    for(int i = 0; i < N; i++){
+        string temp;
+        cin >> temp;
+        for(int j = 0; j < M; j++){
+            board[i][j] = temp[j];
+            if(temp[j] == 'R'){
+                red_x = i;
+                red_y = j;
+            }
+            else if(temp[j] == 'B'){
+                blue_x = i;
+                blue_y = j;
+            }
+        }
+    }
 
     return 0;
 }
