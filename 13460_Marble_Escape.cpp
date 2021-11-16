9053ba269168ce55e5fce30c758dd1f6d50b4405
pdh93621 <hyenp1993@gmail.com>
2021-11-17 02:16:57 +0900

    no message

--- 13460_Marble_Escape.cpp
+++ 13460_Marble_Escape.cpp
@@ -9,160 +9,92 @@ char board[10][10];
 int red_x, red_y, blue_x, blue_y;
 int goal_x, goal_y;
 
-int result = 1001;
+int result = 11;
 
-void move_left(){
-    int n = 0;
-    int m = 0;
-    if(red_y <= blue_y){
-        while(board[red_x][red_y - n - 1] == '.'){
-            n++;
-        }
-        board[red_x][red_y] = '.';
-        board[red_x][red_y - n] = 'R';
+int dir_x[4] = {-1, 0, 0, 1};
+int dir_y[4] = {0, -1, 1, 0};
 
-        while(board[blue_x][blue_y - m - 1] == '.'){
-            m++;
+void look_board(){
+    for(int i = 0; i < N; i++){
+        for(int j = 0; j < M; j++){
+            cout << board[i][j] << " ";
         }
-        board[blue_x][blue_y] = '.';
-        board[blue_x][blue_y - m] = 'B';
+        cout << "\n";
     }
-    else{
-        while(board[blue_x][blue_y - m - 1] == '.'){
-            m++;
-        }
-        board[blue_x][blue_y] = '.';
-        board[blue_x][blue_y - m] = 'B';
-
-        while(board[red_x][red_y - n - 1] == '.'){
-            n++;
-        }
-        board[red_x][red_y] = '.';
-        board[red_x][red_y - n] = 'R';
-    }
-    red_y -= n;
-    blue_y -= m;
+    cout << "\n";
 }
 
-void move_right(){
-    int n = 0;
-    int m = 0;
-    if(red_y >= blue_y){
-        while(board[red_x][red_y + n + 1] == '.'){
-            n++;
-        }
-        board[red_x][red_y] = '.';
-        board[red_x][red_y + n] = 'R';
-
-        while(board[blue_x][blue_y + m + 1] == '.'){
-            m++;
-        }
-        board[blue_x][blue_y] = '.';
-        board[blue_x][blue_y + n] = 'B';
+void move_red(int dir){
+    int n = 1;
+    while(board[red_x + n * dir_x[dir]][red_y + n * dir_y[dir]] == '.'){
+        n++;
     }
-    else{
-        while(board[blue_x][blue_y + m + 1] == '.'){
-            m++;
-        }
-        board[blue_x][blue_y] = '.';
-        board[blue_x][blue_y + m] = 'B';
-
-        while(board[red_x][red_y + n + 1] == '.'){
-            n++;
-        }
-        board[red_x][red_y] = '.';
-        board[red_x][red_y + n] = 'R';
-    }
-    red_y += n;
-    blue_y += m;
+    n--;
+    board[red_x][red_y] = '.';    
+    red_x += n * dir_x[dir];
+    red_y += n * dir_y[dir];
+    board[red_x][red_y] = 'R';
 }
 
-void move_up(){
-    int n = 0;
-    int m = 0;
-    if(red_x <= blue_x){
-        while(board[red_x - n - 1][red_y] == '.'){
-            n++;
-        }
-        board[red_x][red_y] = '.';
-        board[red_x - n][red_y] = 'R';
-
-        while(board[blue_x - m - 1][blue_y] == '.'){
-            m++;
-        }
-        board[blue_x][blue_y] = '.';
-        board[blue_x - m][blue_y] = 'B';
+void move_blue(int dir){
+    int n = 1;
+    while(board[blue_x + n * dir_x[dir]][blue_y + n * dir_y[dir]] == '.'){
+        n++;
     }
-    else{
-        while(board[blue_x - m - 1][blue_y] == '.'){
-            m++;
-        }
-        board[blue_x][blue_y] = '.';
-        board[blue_x - m][blue_y] = 'B';
-        
-        while(board[red_x - n - 1][red_y] == '.'){
-            n++;
-        }
-        board[red_x][red_y] = '.';
-        board[red_x - n][red_y] = 'R';
-    }
-    red_x -= n;
-    blue_x -= m;
+    n--;
+    board[blue_x][blue_y] = '.';
+    blue_x += n * dir_x[dir];
+    blue_y += n * dir_y[dir];
+    board[blue_x][blue_y] = 'B';
 }
 
-void move_down(){
-    int n = 0;
-    int m = 0;
-    if(red_x >= blue_x){
-        while(board[red_x + n + 1][red_y] == '.'){
-            n++;
-        }
-        board[red_x][red_y] = '.';
-        board[red_x + n][red_y] = 'R';
-
-        while(board[blue_x + m + 1][blue_y] == '.'){
-            m++;
-        }
-        board[blue_x][blue_y] = '.';
-        board[blue_x + m][blue_y] = 'B';
-    }
-    else{
-        while(board[blue_x + m + 1][blue_y] == '.'){
-            m++;
-        }
-        board[blue_x][blue_y] = '.';
-        board[blue_x + m][blue_y] = 'B';
-        
-        while(board[red_x + n + 1][red_y] == '.'){
-            n++;
-        }
-        board[red_x][red_y] = '.';
-        board[red_x + n][red_y] = 'R';
-    }
-    red_x += n;
-    blue_x += m;
-
+void move_board(int dir){
+    move_red(dir);
+    move_blue(dir);
+    move_red(dir);
+    //look_board();
 }
 
-void shake(int dir){
-    if(dir == 0){
-        move_left();
+void dfs(int n, int dir){
+    if(n >= result){
+        return;
     }
-    else if(dir == 1){
-        move_up();
-    }
-    else if(dir == 2){
-        move_down();
-    }
-    else if(dir == 3){
-        move_right();
+
+    int temp_rx = red_x;
+    int temp_ry = red_y;
+    int temp_bx = blue_x;
+    int temp_by = blue_y;
+
+    move_board(dir);
+    
+    if(temp_rx != red_x || temp_ry != red_y || temp_bx != blue_x || temp_by != blue_y){
+        if(min(temp_bx, blue_x) > goal_x || max(temp_bx, blue_x) < goal_x || min(temp_by, blue_y) > goal_y || max(temp_by, blue_y) < goal_y){
+            if(min(temp_rx, red_x) <= goal_x && max(temp_rx, red_x) >= goal_x && min(temp_ry, red_y) <= goal_y && max(temp_ry, red_y) >= goal_y){
+                board[red_x][red_y] = '.';
+                move_blue(dir);
+                if(min(temp_bx, blue_x) > goal_x || max(temp_bx, blue_x) < goal_x || min(temp_by, blue_y) > goal_y || max(temp_by, blue_y) < goal_y){
+                    result = n;
+                }                
+            }
+            else{
+                for(int i = 1; i <= 3; i++){
+                    dfs(n + 1, (dir + i) % 4);
+                }
+            }
+        }
+
+        board[red_x][red_y] = '.';
+        board[temp_rx][temp_ry] = 'R';
+        board[blue_x][blue_y] = '.';
+        board[temp_bx][temp_by] = 'B';
+        red_x = temp_rx;
+        red_y = temp_ry;
+        blue_x = temp_bx;
+        blue_y = temp_by;
     }
 }
-
-
 
 int main(){
-    char dirs[4] = {'L', 'U', 'D', 'R'};
     cin >> N >> M;
 
     int org_rx;
@@ -190,13 +122,16 @@ int main(){
             else if(temp[j] == 'O'){
                 goal_x = i;
                 goal_y = j;
-                temp[j] = '.';
+                board[i][j] = '.';
             }
         }
     }
 
+    for(int i = 0; i < 4; i++){
+        dfs(1, i);
+    }
 
-
+    //look_board();
     if(result > 10) result = -1;
 
     cout << result << endl;
