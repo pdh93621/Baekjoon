27926a942229c38a5fcfb3003d71da4d2fe8859b
pdh93621 <hyenp1993@gmail.com>
2021-11-18 23:53:25 +0900

    no message

--- 12100_2048(Easy).cpp
+++ 12100_2048(Easy).cpp
@@ -1,15 +1,23 @@
 #include <iostream>
+#include <stack>
+#include <utility>
 
 using namespace std;
 
 int board[20][20];
+int N;
 
 int x_dir[4] = {-1, 0, 0, 1};
 int y_dir[4] = {0, -1, 1, 0};
 
+void move(int dir){
+    stack<pair<int, bool>> temp;
+    
+}
+
 int main(){
 
-    int N;
+    
     for(int i = 0; i < N; i++){
         for(int j = 0; j < N; j++){
             cin >> board[i][j];
