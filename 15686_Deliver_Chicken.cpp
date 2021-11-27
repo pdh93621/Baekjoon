24ba7b7b6e162e32ae2f609dfb075c59aaf2ba76
pdh93621 <hyenp1993@gmail.com>
2021-11-27 11:23:14 +0900

    no message

--- 15686_Deliver_Chicken.cpp
+++ 15686_Deliver_Chicken.cpp
@@ -8,8 +8,9 @@ vector<pair<int, int>> house;
 vector<pair<int, int>> kfc;
 int h_num;
 int k_num;
-int M[13];
+int selection[14] = {0,};
 vector<vector<int>> dist;
+int result = 10001;
 
 int get_distance(pair<int, int> k, pair<int, int> h) {
     return abs(h.first - k.first) + abs(h.second - k.second);
@@ -24,7 +25,27 @@ void k2h(){
     }
 }
 
+void sigma_k2h(int n){
+    if(n == M + 1){
+        int temp;
+        int sigma = 0;
+        for(int j = 0; j < h_num; j++){
+            temp = 101;
+            for(int i = 1; i <= M; i++){
+                temp = min(temp, dist[selection[i]][j]);
+            }
+            sigma += temp;
+        }
+        result = min(sigma, result);
+    
+        return;
+    }
 
+    for(int i = selection[n - 1] + 1; i < k_num; i++){
+        selection[n] = i;
+        sigma_k2h(n + 1); 
+    }
+}
 
 int main(){
     int temp;
@@ -39,7 +60,12 @@ int main(){
     h_num = house.size();
     k_num = kfc.size();
 
+    selection[0] = -1;
+
     k2h();
 
+    sigma_k2h(1);
+
+    cout << result << endl;
     return 0;
 }
