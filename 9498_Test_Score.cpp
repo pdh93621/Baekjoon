7928066c784f85f9ccb104934d331ca4a404c7f9
pdh93621 <hyenp1993@gmail.com>
2021-12-30 21:50:43 +0900

    no message

---
+++ 9498_Test_Score.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int A;
+    cin >> A;
+
+    switch (A / 10)
+    {
+    case 10:
+    case 9:
+        cout << "A" << endl;
+        break;
+    case 8:
+        cout << "B" << endl;
+        break;
+    case 7:
+        cout << "C" << endl;
+        break;
+    case 6:
+        cout << "D" << endl;
+        break;    
+    default:
+        cout << "F" << endl;
+        break;
+    }
+
+    return 0;
+}
